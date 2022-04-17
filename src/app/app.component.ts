8285ea7571edff0db164af82c383d5dbfe4b0b41
brewzk1 <brewzkee1@gmail.com>
2022-04-16 21:15:19 -0500

    added example 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { ApiService } from './api.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng13-async-pipe';
+   example1userData: any[] = [];
+   example1Sub: Subscription = new Subscription();
+
+   constructor(private apiService: ApiService) { }
+
+   ngOnInit() {
+      this.example1Sub = this.apiService.getUsers()
+         .subscribe((res: any) => {
+            this.example1userData = res;
+         });
+   }
+
+   ngOnDestroy() {
+      this.example1Sub.unsubscribe();
+   }
 }
